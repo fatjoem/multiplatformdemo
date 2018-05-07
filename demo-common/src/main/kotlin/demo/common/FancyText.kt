ca7897b08df15696231a3fe268b15827c133b80c
Fatih Coşkun <fatih.coskun@markt.de>
2018-05-07 15:28:12 +0200

    Updated comment that describes how IntelliJ behaves incorrectly.

--- demo-common/src/main/kotlin/demo/common/FancyText.kt
+++ demo-common/src/main/kotlin/demo/common/FancyText.kt
@@ -1,5 +1,11 @@
 package demo.common
 
+/*
+ * When enabling IntelliJ File | Settings | Build, Execution, Deployment | Compiler | Build project automatically,
+ * then IntelliJ will complain with 'Error:(9, 35) Kotlin: Unresolved reference: String' in its 'Problems' tool window.
+ *
+ * However this file compiles successfully with gradle.
+ */
 class FancyText(private val text: String) {
   override fun toString() = "Fancy: $text"
 }
--- demo-jvm/src/main/java/demo/Test.java
+++ demo-jvm/src/main/java/demo/Test.java
@@ -6,8 +6,6 @@ public class Test {
   public static void main(String[] args) {
     final FancyText fancyText = new FancyText("Test");
 
-    // intellij shows an error for the following line:
-    // (though it compiles and runs successfully with gradle)
     final String string = fancyText.toString();
     System.out.println(string);
   }
