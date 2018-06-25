391bb5e6460f0010b2a1e0534f25c7908d2bf884
Fatih Coşkun <fatih.coskun@markt.de>
2018-06-25 13:21:39 +0200

    Demonstrate invoking top level function from dependency module.

--- demo-common/src/main/kotlin/demo/common/DemoCommon.kt
+++ demo-common/src/main/kotlin/demo/common/DemoCommon.kt
@@ -1,10 +1,11 @@
 package demo.common
 
 import dependency.DependencyCommon
+import dependency.dependencyCommon
 
 class DemoCommon {
 
   private val dep = DependencyCommon()
 
-  override fun toString(): String = "I am a common demo $dep"
+  override fun toString(): String = "I am a common demo $dep ${dependencyCommon()}"
 }
